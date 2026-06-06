850fe513839fe6ebe0a6c201cacea8d133fe5dbd
DonartSefa <donart.sefa13@gmail.com>
2026-06-06 12:15:29 +0200

    d

--- module18/ds.py
+++ module18/ds.py
@@ -12,44 +12,51 @@ import plotly.express as px
 # })
 #
 # df
-
-
-books_df = pd.read_csv("bestsellers_with_categories_2022_03_27.csv")
-
-
+filtered_books_df = pd.read_csv("bestsellers_with_categories_2022_03_27.csv")
 st.sidebar.header("Add new book data")
 with st.sidebar.form("Book_form"):
     new_name = st.text_input("Book Name")
     new_author = st.text_input("Author")
-    new_user_rating =  st.slider("User Rating",0.0,5.0,0.0,0.1)
-    new_reviews = st.number_input("Reviews",min_value=0, step=1)
+    new_user_rating = st.slider("user rating", min_value=0.0, max_value=5.0, value=0.0,step=0.1)
+    new_reviews= st.number_input("Reviews", min_value=0,step=1)
     new_price = st.number_input("Price", min_value=0, step=1)
-    new_year = st.number_input("Year",min_value=2009,max_value=2022,step=1)
-    new_genre = st.selectbox("Genre",books_df['Genre'].unique())
+    new_year = st.number_input("Year", min_value=2009,max_value=2022, step=1)
+    new_genre = st.selectbox("Genre", filtered_books_df["Genre"].unique())
     submit_button = st.form_submit_button(label="Add Book")
 
 if submit_button:
     new_data = {
-        "Name": new_name,
-        "Author": new_author,
-        "User Rating": new_user_rating,
-        "Reviews": new_reviews,
-        "Price": new_price,
-        "Year": new_year,
-        "Genre" :new_genre
+        'Name': new_name,
+        'Author': new_author,
+        'User Rating': new_user_rating,
+        'Reviews': new_reviews,
+        'Price': new_price,
+        'Year': new_year,
+        'Genre': new_genre
     }
+    filtered_books_df = pd.concat([pd.DataFrame(new_data, index=[0]), filtered_books_df], ignore_index=True)
+    filtered_books_df.to_csv('bestsellers_with_categories_2022_03_27.csv',index=False)
+    st.sidebar.success("New book added")
 
-
-    books_df = pd.concat([pd.DataFrame(new_data, index=[0]), books_df],ignore_index = True)
-    books_df.to_csv('bestsellers_with_categories_2022_03_27.csv')
-    st.sidebar.success("New Book added successfully")
 
 st.sidebar.header("Filter Options")
-selected_author = st.sidebar.selectbox("Select Author",["ALl"]+list(books_df['Author'].unique()))
-selected_year = st.sidebar.selectbox("Select Year",["ALl"]+list(books_df['Year'].unique()))
-selected_genre = st.sidebar.selectbox("Select Genre",["ALl"]+list(books_df['Genre'].unique()))
-min_rating = st.sidebar.slider("Minimum User rating",0.0,5.0,0.0,0.1)
-max_price = st.sidebar.slider("Max price",0, books_df['Price'].max(),books_df['Price'].max())
+selected_author = st.sidebar.selectbox("Select Author", ["All"]+list(filtered_books_df['Author'].unique()))
+selected_year = st.sidebar.selectbox("Select Year", ["All"]+list(filtered_books_df['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre", ["All"]+list(filtered_books_df['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum User Rating",0.0,5.0,0.0,0.1)
+max_price = st.sidebar.slider("Max Price",0,filtered_books_df['Price'].max(),filtered_books_df['Price'].max())
+
+filtered_books_df = filtered_books_df.copy()
+if selected_author != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Author'] == selected_author]
+if selected_year != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Year'] == int(selected_year)]
+if selected_genre != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Genre'] == selected_genre]
+
+filtered_books_df = filtered_books_df[(filtered_books_df['User Rating'] >= min_rating) & (filtered_books_df['Price'] <= max_price)]
+
+
 
 
 
@@ -59,10 +66,11 @@ st.write("This app analyzes the Amazon topselling books")
 
 
 st.subheader("Summary Statistics")
-total_books = books_df.shape[0]
-unique_titles = books_df['Name'].nunique()
-avg_rating = books_df["User Rating"].mean()
-avg_price = books_df['Price'].mean()
+total_books = filtered_books_df.shape[0]
+unique_titles = filtered_books_df['Name'].nunique()
+avg_rating = filtered_books_df["User Rating"].mean()
+avg_price = filtered_books_df['Price'].mean()
+
 
 col1,col2,col3,col4 = st.columns(4)
 col1.metric("Total books", total_books)
@@ -71,21 +79,21 @@ col3.metric("Average Rating",avg_rating)
 col4.metric("Average Price",avg_price)
 
 st.subheader("Dataset Preview")
-st.write(books_df.head())
+st.write(filtered_books_df.head())
 
-
-col1,col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Top 10 Book Titles")
-    top_titles = books_df["Name"].value_counts().head(10)
+    top_titles = filtered_books_df['Name'].value_counts().head(10)
     st.bar_chart(top_titles)
 
 with col2:
-    st.subheader("Top 10 Authors")
-    top_authors = books_df["Author"].value_counts().head(10)
-    st.bar_chart(top_authors)
+    st.subheader("Top 10 Book Author")
+    top_author = filtered_books_df['Author'].value_counts().head(10)
+    st.bar_chart(top_author)
+
 
 st.subheader("Genre Pie Chart")
-fig = px.pie(books_df, names="Genre",title="Most liked Genre",color_discrete_sequence=px.colors.sequential.Plasma)
+fig = px.pie(filtered_books_df, names="Genre", title="Most Liked Genre", color="Genre",color_discrete_sequence= px.colors.sequential.Plasma)
 st.plotly_chart(fig)
